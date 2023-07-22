53500249e0913639d635b044e54ab22b0f043058
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-22 16:25:13 +0700

    fix: error css

--- src/Components/styledComponent/Example1.tsx
+++ src/Components/styledComponent/Example1.tsx
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`color: black; background-color: red`;
+export const Container = styled.div`
+background-color: green;
+color: white;
+display: flex;
+justify-content: center;
+`;
+
+export const Title = styled.h1`
+color: black; 
+background-color: red`;
--- src/Components/styledComponent/HelloWorld.tsx
+++ src/Components/styledComponent/HelloWorld.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Title } from "./Example1";
 
 export const HelloWorld = () => {
