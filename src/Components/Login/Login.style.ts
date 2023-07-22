fe022aa2acf5f3cb9824a7308c020435334b578a
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-22 18:27:46 +0700

    feat: style login

--- src/Components/Login/Login.style.ts
+++ src/Components/Login/Login.style.ts
@@ -2,6 +2,37 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 display: flex;
+height: 100vh;
+padding: 0 20px;
+flex-direction: column;
+justify-content: center;
+align-items: start;
+gap: 15px;
+.items-container1{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    padding: 10px 15px;
+    gap:15px;
+    h1{
+        margin: 0;
+        padding:0
+     }
+    p{
+        margin: 0;
+        padding:0;
+        a {
+        color:blue;
+        text-decoration: none;
+     }
+    }
+}
 
+.items-container2{
+    background-color: white;
+}
+`;
 
+export const Form = styled.form`
+ background-color:red;
 `;
--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,9 +1,20 @@
 import { Container } from "./Login.style";
+import { Form } from "./Login.style";
+import Google from "../../../public/img/google.svg";
 
 export const Login = () => {
    return (
       <Container>
-         <h1>Halo DUnia</h1>
+         <div className='items-container1'>
+            <h1>Daftar</h1>
+            <p>
+               Sudah punya? <a href='/'>Masuk</a>{" "}
+            </p>
+         </div>
+         <div className='items-container2'>
+            <img src={Google} alt='google' />
+         </div>
+         <Form>j</Form>
       </Container>
    );
 };
