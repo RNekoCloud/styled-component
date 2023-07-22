eda30f8fdf6989d2e8a5785ec3fe6839db8002ef
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-22 17:30:33 +0700

    feat: try to stye with styled component

--- src/Components/styledComponent/Example1.tsx
+++ src/Components/styledComponent/Example1.tsx
@@ -3,11 +3,59 @@ import styled from "styled-components";
 export const Container = styled.div`
 background-color: green;
 color: white;
- font-family: 'Courier New', Courier, monospace;
+font-family: 'Courier New', Courier, monospace;
 display: flex;
+padding: 10px 20px;
 justify-content: center;
+flex-direction: column;
+align-items: center;
+`;
+
+export const ItemsContainer = styled.div`
+background-color: green;
+color: white;
+font-family: 'Courier New', Courier, monospace;
+display: flex;
+gap: 6px;
+padding: 0 20px;
+justify-content: center;
+flex-direction: row;
+align-items: center;
 `;
 
 export const Title = styled.h1`
-font-size: 20px;
+font-size: 40px;
 font-weight: 900`;
+
+export const Paragraph = styled.p`
+font-size: 20px;
+`;
+
+export const Button1 = styled.button`
+background-color: red;
+border: 2px solid black;
+border-radius: 10px;
+color: white;
+font-weight: bold;
+padding: 10px 15px;
+
+&:hover{
+    background-color: rgb(240, 170, 170);
+    transform: scale(0.96);
+    color: black;
+}
+`;
+
+export const Button2 = styled.button`
+background-color: white;
+border: 2px solid black;
+border-radius: 10px;
+color: black;
+font-weight: bold;
+padding: 10px 15px;
+&:hover{
+    background-color: red;
+    transform: scale(0.96);
+    color: white;
+}
+`;
--- src/Components/styledComponent/HelloWorld.tsx
+++ src/Components/styledComponent/HelloWorld.tsx
@@ -1,10 +1,24 @@
 import { Title } from "./Example1";
 import { Container } from "./Example1";
+import { Paragraph } from "./Example1";
+import { Button1 } from "./Example1";
+import { Button2 } from "./Example1";
+import { ItemsContainer } from "./Example1";
 
 export const HelloWorld = () => {
    return (
       <Container>
          <Title>Hello World</Title>
+         <Paragraph>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur libero dignissimos unde rem culpa corrupti laudantium
+            voluptatum eligendi, magni soluta iusto vero quas error temporibus
+            tempore quo ipsum repudiandae saepe!
+         </Paragraph>
+         <ItemsContainer>
+            <Button1>Click Me!</Button1>
+            <Button2>Click Saya!</Button2>
+         </ItemsContainer>
       </Container>
    );
 };
