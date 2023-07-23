845804098372c0ab3685f17e553b2531e4596233
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-23 10:33:40 +0700

    style: centering element

--- src/Components/Login/Login.style.ts
+++ src/Components/Login/Login.style.ts
@@ -1,18 +1,21 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const MainContainer = styled.div`
    display: flex;
    height: 100vh;
-   padding: 0 20px;
-   flex-direction: column;
    justify-content: center;
+   align-items: center;
+`;
+
+export const Container = styled.div`
+   display: flex;
+   flex-direction: column;
    align-items: start;
    gap: 15px;
    .items-container1 {
       display: flex;
-      justify-content: center;
+      align-items: start;
       flex-direction: column;
-      padding: 10px 15px;
       gap: 15px;
       h1 {
          margin: 0;
@@ -24,6 +27,9 @@ export const Container = styled.div`
          a {
             color: blue;
             text-decoration: none;
+         }
+         a:hover {
+            text-decoration: underline;
          }
       }
    }
@@ -39,21 +45,35 @@ export const Form = styled.form`
    gap: 25px;
    .input-container1 {
       display: flex;
+      font-weight: bold;
+      gap: 4px;
       flex-direction: column;
    }
    .input-container1 > input {
       border: 2px solid black;
+      width: 20em;
       padding: 10px 5px;
       border-radius: 5px;
    }
 
    .input-container2 {
       display: flex;
+      gap: 4px;
+      font-weight: bold;
       flex-direction: column;
    }
    .input-container2 > input {
       border: 2px solid black;
       padding: 10px 0 8px 5px;
       border-radius: 5px;
+   }
+   button {
+      width: 7em;
+      padding: 10px 0;
+      font-weight: bold;
+      border: none;
+      color: white;
+      background-color: orange;
+      border-radius: 2px;
    }
 `;
--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,30 +1,37 @@
+import { MainContainer } from "./Login.style";
 import { Container } from "./Login.style";
 import { Form } from "./Login.style";
 import Google from "../../../public/img/google.svg";
 
 export const Login = () => {
    return (
-      <Container>
-         <div className='items-container1'>
-            <h1>Daftar</h1>
-            <p>
-               Sudah punya? <a href='/'>Masuk</a>{" "}
-            </p>
-         </div>
-         <div className='items-container2'>
-            <img src={Google} alt='google' />
-         </div>
-         <Form>
-            <div className='input-container1'>
-               <label htmlFor='email'>Email</label>
-               <input type='email' id='email' placeholder='example@gmail.com' />
+      <MainContainer>
+         <Container>
+            <div className='items-container1'>
+               <h1>Daftar</h1>
+               <p>
+                  Sudah punya akun? <a href='/'>Masuk</a>{" "}
+               </p>
             </div>
-            <div className='input-container2'>
-               <label htmlFor='pw'>Kata Sandi</label>
-               <input type='password' id='pw' placeholder='*******' />
+            <div className='items-container2'>
+               <img src={Google} alt='google' />
             </div>
-            <button>Daftar</button>
-         </Form>
-      </Container>
+            <Form>
+               <div className='input-container1'>
+                  <label htmlFor='email'>Email</label>
+                  <input
+                     type='email'
+                     id='email'
+                     placeholder='example@gmail.com'
+                  />
+               </div>
+               <div className='input-container2'>
+                  <label htmlFor='pw'>Kata Sandi</label>
+                  <input type='password' id='pw' placeholder='*******' />
+               </div>
+               <button>Daftar</button>
+            </Form>
+         </Container>
+      </MainContainer>
    );
 };
