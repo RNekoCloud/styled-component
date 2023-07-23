3aa45da0d2a72ce2a6f1bafbc52812644e176ea8
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-23 10:08:08 +0700

    fix: formatter

--- src/Components/Login/Login.style.ts
+++ src/Components/Login/Login.style.ts
@@ -1,58 +1,59 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-display: flex;
-height: 100vh;
-padding: 0 20px;
-flex-direction: column;
-justify-content: center;
-align-items: start;
-gap: 15px;
-.items-container1{
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    padding: 10px 15px;
-    gap:15px;
-    h1{
-        margin: 0;
-        padding:0
-     }
-    p{
-        margin: 0;
-        padding:0;
-        a {
-        color:blue;
-        text-decoration: none;
-     }
-    }
-}
+   display: flex;
+   height: 100vh;
+   padding: 0 20px;
+   flex-direction: column;
+   justify-content: center;
+   align-items: start;
+   gap: 15px;
+   .items-container1 {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      padding: 10px 15px;
+      gap: 15px;
+      h1 {
+         margin: 0;
+         padding: 0;
+      }
+      p {
+         margin: 0;
+         padding: 0;
+         a {
+            color: blue;
+            text-decoration: none;
+         }
+      }
+   }
 
-.items-container2{
-    background-color: white;
-}
+   .items-container2 {
+      background-color: white;
+   }
 `;
 
 export const Form = styled.form`
- display: flex;
- flex-direction: column;
- gap:25px;
- .input-container1{
-    display: flex;
-    flex-direction: column;
- }
-.input-container1 > input{
-    border: 2px solid black;
-    padding: 10px 5px
- }
+   display: flex;
+   flex-direction: column;
+   gap: 25px;
+   .input-container1 {
+      display: flex;
+      flex-direction: column;
+   }
+   .input-container1 > input {
+      border: 2px solid black;
+      padding: 10px 5px;
+      border-radius: 5px;
+   }
 
- .input-container2{
-    display: flex;
-    flex-direction: column;
- }
- .input-container2 > input{
-   border: 2px solid black;
-   padding: 10px 0 8px 5px;
-   border-radius: 5px;
- }
+   .input-container2 {
+      display: flex;
+      flex-direction: column;
+   }
+   .input-container2 > input {
+      border: 2px solid black;
+      padding: 10px 0 8px 5px;
+      border-radius: 5px;
+   }
 `;
