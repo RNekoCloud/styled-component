b1b991966797b12c36fc9c838b1bf9d680a2de87
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-23 14:19:41 +0700

    style: add Title

--- src/Components/Login/Header.style.ts
+++ src/Components/Login/Header.style.ts
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-   background-color: green;
+   background-color: #0d4a3e;
    margin: 0;
    height: 100vh;
+   display: flex;
+   justify-content: center;
+`;
+
+export const Title = styled.div`
+   gap: 13px;
+   color: white;
+   display: flex;
+   align-items: center;
 `;
--- src/Components/Login/Header.tsx
+++ src/Components/Login/Header.tsx
@@ -1,5 +1,14 @@
 import { Wrapper } from "./Header.style";
+import { Title } from "./Header.style";
+import CvIcon from "../../../public/img/Cv.svg";
 
 export const Header = () => {
-   return <Wrapper>halo00000</Wrapper>;
+   return (
+      <Wrapper>
+         <Title>
+            <img src={CvIcon} alt='Cv Zaman Now' />
+            <h2>CV Zaman Now</h2>
+         </Title>
+      </Wrapper>
+   );
 };
