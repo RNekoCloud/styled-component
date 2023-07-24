d414a1a3c3e5f0aa5532d442b911dbf516a53527
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-24 16:16:40 +0700

    style: centering element

--- src/Components/Responsive/Form.tsx
+++ src/Components/Responsive/Form.tsx
@@ -1,3 +1,12 @@
+import { Wrapper } from "./Index.style";
+import MobileImage from "../../../public/img/Login-Art(mobile).svg";
+
 export const Form = () => {
-   return <div>index</div>;
+   return (
+      <Wrapper>
+         <div className='img-container'>
+            <img src={MobileImage} alt='' />
+         </div>
+      </Wrapper>
+   );
 };
