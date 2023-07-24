abd80b2907bd123589b0f1bd0f13fd7b5eda74ce
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-24 16:08:26 +0700

    fix: main route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import { HelloWorld } from "./Components/styledComponent/HelloWorld";
-import { Login } from "./Components/Login/Login";
+// import { Login } from "./Components/Login/Login";
+import { Form } from "./Components/Responsive/Form";
 
 export const App = () => {
    return (
       <div>
          {/* <HelloWorld /> */}
-         <Login />
+         {/* <Login /> */}
+         <Form />
       </div>
    );
 };
