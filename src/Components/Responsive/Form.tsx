cff8d6e9f38b91be62078876219917de47268c25
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-24 16:20:53 +0700

    style: padding

--- src/Components/Responsive/Form.tsx
+++ src/Components/Responsive/Form.tsx
@@ -7,6 +7,13 @@ export const Form = () => {
          <div className='img-container'>
             <img src={MobileImage} alt='' />
          </div>
+         <div className='greeting-container'>
+            <h2>Welcome Back 👋</h2>
+            <p>
+               Today is a new day. It's your day. You shape it. Sign in to start
+               managing your projects.
+            </p>
+         </div>
       </Wrapper>
    );
 };
--- src/Components/Responsive/Index.style.ts
+++ src/Components/Responsive/Index.style.ts
@@ -4,5 +4,9 @@ export const Wrapper = styled.div`
    display: flex;
    align-items: center;
    justify-content: center;
-   padding: 5% 0;
+   padding: 5% 0%;
+   flex-direction: column;
+   .greeting-container {
+      padding: 0 3%;
+   }
 `;
