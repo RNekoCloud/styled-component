e3a631e2de570a8eb981d15537e0491774ec7865
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-25 10:40:23 +0700

    feat: add new example

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 // import { HelloWorld } from "./Components/styledComponent/HelloWorld";
 // import { Login } from "./Components/Login/Login";
 // import { Form } from "./Components/Responsive/Form";
-import { Event } from "./Components/React-Basic/Event";
+import { Event2 } from "./Components/React-Basic/Event2";
 
 export const App = () => {
    return (
       <div>
          {/* <HelloWorld /> */}
          {/* <Login /> */}
-         <Event />
+         <Event2 />
       </div>
    );
 };
---
+++ src/Components/React-Basic/Event2.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Event2 = () => {
+   return <div>Event2</div>;
+};
