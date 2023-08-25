19ed1cca6f62276fd8801cf3e4f470781165adee
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-08-25 21:09:36 +0700

    feat: add vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
-// vite.config.js
-import { resolve } from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-   build: {
-      lib: {
-         // Could also be a dictionary or array of multiple entry points
-         entry: resolve(__dirname, "lib/main.js"),
-         name: "MyLib",
-         // the proper extensions will be added
-         fileName: "my-lib",
-      },
-      rollupOptions: {
-         // make sure to externalize deps that shouldn't be bundled
-         // into your library
-         external: ["vue"],
-         output: {
-            // Provide global variables to use in the UMD build
-            // for externalized deps
-            globals: {
-               vue: "React",
-            },
-         },
-      },
-   },
+   plugins: [react()],
 });
