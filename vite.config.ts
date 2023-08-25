845581d0231384488d36d69fff86d619b0b5bf0c
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-08-25 19:09:59 +0700

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,18 @@ export default defineConfig({
    build: {
       lib: {
          // Could also be a dictionary or array of multiple entry points
-         entry: resolve(__dirname, "lib/index.html"),
+         entry: resolve(__dirname, "lib/main.js"),
          name: "MyLib",
          // the proper extensions will be added
          fileName: "my-lib",
       },
       rollupOptions: {
+         input: {
+            main: resolve(__dirname, "index.htl"),
+         },
          // make sure to externalize deps that shouldn't be bundled
          // into your library
-         external: ["react"],
+         external: ["vue"],
          output: {
             // Provide global variables to use in the UMD build
             // for externalized deps
